e99531c4d4bc1e4d0a52417c81901023d693b72d
Victor Jesus Goyzueta Argandona <vgoyzuet@c2r1s6.42madrid.com>
2026-06-18 15:10:11 +0200

    update

--- cpp-08/ex00/easyfind.hpp
+++ cpp-08/ex00/easyfind.hpp
@@ -6,6 +6,9 @@
 template<typename T>
 typename T::iterator easyfind(T& container, const int i);
 
+template<typename T>
+typename T::const_iterator easyfind(const T& container, const int value);
+
 # include "easyfind.tpp"
 
 #endif
