bfb858b389cd6e5422d68a930cda650c94f53c5e
Victor Jesus Goyzueta Argandona <vgoyzuet@c2r1s6.42madrid.com>
2026-06-18 17:26:36 +0200

    update

---
+++ cpp-08/ex01/Span.hpp
@@ -0,0 +1,42 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <iterator>
+# include <algorithm>
+# include <vector>
+# include <stdexcept>
+# include <exception>
+
+class Span
+{
+	private:
+		unsigned int		_maxSize;
+		std::vector<int>	_numbers;
+
+	public:
+		Span();
+		Span( unsigned int n );
+		Span( const Span& copy );
+		~Span();
+
+		Span& operator=( const &Span& other );
+		
+		void	addNumber( int number );
+
+		template <typename Iterator>
+		void	addRange( Iterator begin, Iterator end )
+		{
+			unsigned int	distance = std::distance(begin, end);
+			if (_numbers.size() + distance > _maxSize)
+				throw std::runtime_error("Cannot add range: Span will overflow.");
+			_numbers.insert(_numbers.end(), begin, end);
+		}
+
+		unsigned int	shortesSpan() const;
+		unsigned int	longestSpan() const;
+		void			printNumbers() const;
+};
+
+#endif
+
