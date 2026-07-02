ebf727d998a5809a4770da50935601b22faa37c9
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-07-02 19:37:57 +0200

    update cpp08

---
+++ cpp-08/ex02/MutantStack.hpp
@@ -0,0 +1,46 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <vector>
+# include <list>
+# include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+
+	public:
+		MutantStack();
+		MutantStack( const MutantStack& copy );
+		~MutantStack();
+
+		MutantStack&	operator=( const MutantStack& other );
+
+		typedef typename std::stack<T>::container_type::iterator
+			iterator;
+		typedef typename std::stack<T>::container_type::const_iterator
+			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator
+			reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator
+			const_reverse_iterator;
+
+		iterator				begin();
+		iterator				end();
+
+		const_iterator			begin() const;
+		const_iterator			end() const;
+
+		reverse_iterator		rbegin();
+		reverse_iterator		rend();
+
+		const_reverse_iterator	rbegin() const;
+		const_reverse_iterator	rend() const;
+};
+
+# include "MutantStack.tpp"
+
+#endif
