26a1dc1e84ee50527d28d13e0aa0696af3d22a18
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-07-21 16:37:54 +0200

    update cpp08/ex02/main.cpp

--- cpp-08/ex02/main.cpp
+++ cpp-08/ex02/main.cpp
@@ -37,3 +37,39 @@ int main()
 
 	return 0;
 }
+
+// int main()
+// {
+// 	std::deque<int> mstack;
+
+// 	mstack.push_back(5);
+// 	mstack.push_back(17);
+
+// 	std::cout << mstack[1] << std::endl;
+
+// 	mstack.pop_back();
+
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	//[...]
+// 	mstack.push_back(0);
+
+// 	std::deque<int>::iterator it = mstack.begin();
+// 	std::deque<int>::iterator ite = mstack.end();
+
+// 	++it;
+// 	--it;
+
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+
+// 	std::stack<int> s(mstack);
+
+// 	return 0;
+// }
