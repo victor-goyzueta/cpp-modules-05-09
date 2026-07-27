921b1e36725b403795385c8d8e11d02d23fa53c9
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-07-27 20:51:03 +0200

    update

--- cpp-09/ex00/inc/BitcoinExchange.hpp
+++ cpp-09/ex00/inc/BitcoinExchange.hpp
@@ -4,15 +4,21 @@
 # include <iostream>
 # include <string>
 # include <map>
+# include <stdexcept>
+# include <fstream>
+# include <sstream>
+// # include <cstdlib>
+// # include <iomanip>
+// # include <limits>
 
 class BitcoinExchange
 {
 	private:
 		BitcoinExchange( const BitcoinExchange& copy );
 		BitcoinExchange& operator=( const BitcoinExchange& other );
-
+		
 		std::map<std::string, double>	_database;
-
+	
 	public:
 		BitcoinExchange();
 		~BitcoinExchange();
--- cpp-09/ex00/src/BitcoinExchange.cpp
+++ cpp-09/ex00/src/BitcoinExchange.cpp
@@ -5,11 +5,46 @@
 
 BitcoinExchange::BitcoinExchange() {}
 
+BitcoinExchange::BitcoinExchange( const BitcoinExchange& copy )
+{
+	(void)copy;
+}
+
+BitcoinExchange& BitcoinExchange::operator=( const BitcoinExchange& other )
+{
+	(void)other;
+	return (*this);
+}
+
 BitcoinExchange::~BitcoinExchange() {}
 
 void	BitcoinExchange::loadDatabase( const std::string &filename )
 {
-	//
+	std::ifstream	file(filename.c_str());
+	std::string		line;
+	
+	if (!file.is_open())
+		throw std::runtime_error("Could not open file");
+	std::getline(file, line);
+	if (!line.c_str())
+		throw std::runtime_error("Empty file");
+	while (std::getline(file, line))
+	{
+		std::string	date, priceStr;
+		double		price;
+
+		size_t pos = line.find(',');
+		if (pos == std::string::npos)
+			continue ;
+		date = line.substr(0, pos);
+		std::string priceStr = line.substr(pos + 1);
+
+		char *end;
+		price = std::strtod(priceStr.c_str(), &end);
+		if (!(*end != '\0' && *end != '\n' && *end != '\r'))
+			_database[date] = price;
+	}
+	file.close();
 }
 
 void	BitcoinExchange::processInput( const std::string &filename )
